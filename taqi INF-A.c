a3baff260cdd116785ddb3a106aecb5fa3029090
Md Taqi Ur Rahaman <mohdtaqiurrahaman@gmail.com>
2026-09-23 16:26:00 +0530

    Add files via upload

---
+++ taqi INF-A.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main
+{
+
+    int a=10, b=20;
+    printf("%d,a>b");
+    printf("%d,a<b");
+    printf("%d,a=b");
+    printf("%d,a==b");
+    printf("%d,a!=b");
+
+
+
+
+    return 0;
+
+
+}
