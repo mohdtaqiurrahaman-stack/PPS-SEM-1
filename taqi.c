aa54522725e1916bacbc519155b7e35de08aa87d
Md Taqi Ur Rahaman <mohdtaqiurrahaman@gmail.com>
2026-09-23 16:14:51 +0530

    Add files via upload

---
+++ taqi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    a=1;
+    b=77;
+
+    printf("%d\n", a > b);
+    printf("%d\n", a < b);
+    printf("%d\n", a == b);
+    printf("%d\n", a != b);
+
+    return 0;
+}
